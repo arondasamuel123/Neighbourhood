e8bb3f9fc446f286398db9adb8d6afc0c7689ec6
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-24 13:08:48 +0300

    Created custom User model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    
+    SUPER_ADMIN = 'SUPER ADMIN'
+    ADMIN = 'ADMIN'
+    USER = 'USER'
+    ROLE_TYPE_CHOICES = [
+        (SUPER_ADMIN, 'SUPER ADMIN'),
+        (ADMIN, 'ADMIN'),
+        (USER, 'USER')
+    ]
+    name = models.CharField(max_length=20)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    role_type = models.CharField(max_length=2, choices=ROLE_TYPE_CHOICES, default=USER)
+    
+    
+    
+class Neighbourhood(models.Model):
+    pass
