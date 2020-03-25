a833628a6f01396936b5cb93b817c75c678eba24
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-25 13:42:25 +0300

    Created User serializer for User model

---
+++ hood/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields =('username','email','name' 'password', 'role_type')
+        
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+        
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,19 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hood'
+    'hood',
+    'rest_framework',
+    'corsheaders'
+    
 ]
 AUTH_USER_MODEL = 'hood.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
